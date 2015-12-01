2dcaa1fffec301b4cff15db697fd4cf41b1732de
Philippa Johnstone <pecj@hotmail.co.uk>
2015-12-01 21:05:54 +0800

    initial commit

---
+++ frontend/js/criminalsController.js
@@ -0,0 +1,25 @@
+angular.module('TheCriminalsApp', [])
+  .controller('CriminalsController', ['$scope', '$http', function CriminalsController ($scope, $http){
+
+    $scope.all = [];
+    $scope.newCriminal = {};
+
+    getCriminals();
+    function getCriminals() {
+      $http
+        .get('http://localhost:3000/criminals')
+        .success(function (data){
+          $scope.all = data.criminals
+        });
+    }
+
+    function addCriminal() {
+    $http
+      .post('http://localhost:3000/criminals', $scope.newCriminal)
+      .success(function (data){
+        getCriminals();
+      });
+    $scope.newCriminal = {};
+    }
+
+  }])
